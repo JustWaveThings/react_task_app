98f40ec3072a0ab744895f0bee44472a0194e470
JustWaveThings <justwavethings@proton.me>
2023-04-15 14:20:53 -0400

    Base TOP solution

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,21 +1,57 @@
-import './App.css';
-import Overview from './components/Overview';
 import React, { Component } from 'react';
+import Overview from './components/Overview';
+import uniqid from 'uniqid';
+import './App.css';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { inputValue: '' };
-		this.handleChange = this.handleChange.bind(this);
+	constructor() {
+		super();
+
+		this.state = {
+			task: {
+				text: '',
+				id: uniqid(),
+			},
+			tasks: [],
+		};
 	}
 
-	handleChange(event) {
-		this.setState({ inputValue: event.target.value });
-	}
+	handleChange = e => {
+		this.setState({
+			task: {
+				text: e.target.value,
+				id: this.state.task.id,
+			},
+		});
+	};
+
+	onSubmitTask = e => {
+		e.preventDefault();
+		this.setState({
+			tasks: this.state.tasks.concat(this.state.task),
+			task: {
+				text: '',
+				id: uniqid(),
+			},
+		});
+	};
+
 	render() {
+		const { task, tasks } = this.state;
+
 		return (
 			<div className="App">
-				<Overview label="Enter Task" />
+				<form onSubmit={this.onSubmitTask}>
+					<label htmlFor="taskInput">Enter task</label>
+					<input
+						onChange={this.handleChange}
+						value={task.text}
+						type="text"
+						id="taskInput"
+					/>
+					<button type="submit">Add Task</button>
+				</form>
+				<Overview tasks={tasks} />
 			</div>
 		);
 	}
--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -1,46 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Overview extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { inputValue: '', values: [] };
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-	handleChange = event => {
-		this.setState({ inputValue: event.target.value });
-	};
-	handleSubmit = event => {
-		event.preventDefault();
-		const newValue = this.state.inputValue;
-		this.setState(prevState => ({
-			values: [...prevState.values, newValue],
-			inputValue: '',
-		}));
-	};
+const Overview = props => {
+	const { tasks } = props;
 
-	render() {
-		return (
-			<div>
-				<h2>{this.props.label}</h2>
-				<form onSubmit={this.handleSubmit}>
-					<label htmlFor="task-input">Add Task</label>
-					<input
-						type="text"
-						id="task-input"
-						value={this.state.inputValue}
-						onChange={this.handleChange}
-					/>
-					<button type="submit">Add Task</button>
-				</form>
-				<ul>
-					{this.state.values.map((value, index) => (
-						<li key={index}>{value}</li>
-					))}
-				</ul>
-			</div>
-		);
-	}
-}
+	return (
+		<ul>
+			{tasks.map(task => {
+				return <li key={task.id}>{task.text}</li>;
+			})}
+		</ul>
+	);
+};
 
 export default Overview;
