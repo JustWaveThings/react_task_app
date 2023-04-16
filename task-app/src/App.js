40695c04b8a56136a400030a1aca69c2f95dc264
JustWaveThings <justwavethings@proton.me>
2023-04-16 10:32:39 -0400

    Back to metal rebuild

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,31 +1,9 @@
 import React, { Component } from 'react';
-import TaskList from './components/TaskList';
 import './App.css';
-import Input from './components/Input';
-import uniqid from 'uniqid';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			task: {
-				text: '',
-				id: uniqid(),
-				index: 0,
-				editing: false,
-			},
-			tasks: [],
-		};
-	}
-
 	render() {
-		return (
-			<>
-				<Input />
-				<TaskList />
-			</>
-		);
+		return <>test</>;
 	}
 }
 
